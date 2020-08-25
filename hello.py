fce16bc60206db1640058e19ea6f6508a960ca88
dronecontrol <dronecontrol@elecspectrum.fr>
2020-08-25 17:22:12 +0200

    test revert

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 print("Hello world")
+<<<<<<< Updated upstream
 print("New conflict :)")
+=======
+<<<<<<< Updated upstream
+=======
+print("New conflict :)")
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
